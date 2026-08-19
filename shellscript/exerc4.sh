84c00e3876ead7c460366fc1158299c165f0ca40
Preti, João Paulo <jppreti@gmail.com>
2026-08-19 10:56:09 -0400

    feat(shellscript): exercício 4 resolvido

---
+++ shellscript/exerc4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# EXERCICIO 4
+
+if [ -f $1 ]; then
+  echo "$1 é um arquivo comum"
+elif [ -d $1 ]; then
+  echo "$1 é um diretório"
+else
+  echo "$1 é alguma outra coisa"
+fi
+
+ls -lh $1
